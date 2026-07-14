8a92018c6a812f07755cbd72226a0d66d96bda05
Shankar-codes <harikainteriors@gmail.com>
2026-07-14 18:00:34 +0530

    Jenkins shared library

--- vars/EKSDeploy.groovy
+++ vars/EKSDeploy.groovy
@@ -1,120 +1,44 @@
-def call(Map configMap){
-
-    pipeline {
-        agent {
-            node {
-                label 'AGENT-1'
-            }
+pipeline {
+    agent {
+        node {
+            label 'AGENT-1'
         }
+    }
 
-        environment { 
-            COURSE = 'Jenkins'
-            appVersion = configMap.get("appVersion")
-            ACC_ID = "367012942501"
-            PROJECT = configMap.get("project")
-            COMPONENT = configMap.get("component")
-            deploy_to = configMap.get("deploy_to")
-        }
+    environment { 
+        COURSE = 'Jenkins'
+        appVersion = ""
+        ACC_ID = "367012942501"
+        PROJECT = "roboshop"
+        COMPONENT = "catalogue"
+    }
 
-        options {
-            timeout(time: 1, unit: 'HOURS') 
-            disableConcurrentBuilds()
-        }
+    options {
+        timeout(time: 1, unit: 'HOURS') 
+        disableConcurrentBuilds()
+    }
 
-        stage('Dependabot Security Gate')
-        {
-            when {
-                expression { false }
-            }
-            environment {
-                GITHUB_OWNER = 'Shankar-codes'
-                GITHUB_REPO  = 'catalogue'
-                GITHUB_API   = 'https://api.github.com'
-                GITHUB_TOKEN = credentials('GITHUB_TOKEN')
-            }
+    // parameters {
+    //     string(name: 'appVersion', description: 'what app version you want to deploy')
+    //     choice(name: 'deploy_to', choices: ['dev','qa','prod'], description: 'pick something')
+    // }
+
+// this is build section. added comment for just webhook checking
+    stages {
+        stage('Deploy') {
             steps {
-                script{
-                    /* Use sh """ when you want to use Groovy variables inside the shell.
-                    Use sh ''' when you want the script to be treated as pure shell. */
-                    sh '''
-                    echo "Fetching Dependabot alerts..."
-
-                    response=$(curl -s \
-                        -H "Authorization: token ${GITHUB_TOKEN}" \
-                        -H "Accept: application/vnd.github+json" \
-                        "${GITHUB_API}/repos/${GITHUB_OWNER}/${GITHUB_REPO}/dependabot/alerts?per_page=100")
-
-                    echo "${response}" > dependabot_alerts.json
-
-                    high_critical_open_count=$(echo "${response}" | jq '[.[] 
-                        | select(
-                            .state == "open"
-                            and (.security_advisory.severity == "high"
-                                or .security_advisory.severity == "critical")
-                        )
-                    ] | length')
-
-                    echo "Open HIGH/CRITICAL Dependabot alerts: ${high_critical_open_count}"
-
-                    if [ "${high_critical_open_count}" -gt 0 ]; then
-                        echo "❌ Blocking pipeline due to OPEN HIGH/CRITICAL Dependabot alerts"
-                        echo "Affected dependencies:"
-                        echo "$response" | jq '.[] 
-                        | select(.state=="open" 
-                        and (.security_advisory.severity=="high" 
-                        or .security_advisory.severity=="critical"))
-                        | {dependency: .dependency.package.name, severity: .security_advisory.severity, advisory: .security_advisory.summary}'
-                        exit 1
-                    else
-                        echo "✅ No OPEN HIGH/CRITICAL Dependabot alerts found"
-                    fi
-                    '''
-                    
-                }
-            }
-        }
-
-        stage('Build Image') {
-            steps {
-                script{
-                    withAWS(region:'us-east-1',credentials:'aws-creds') {
+                withAWS(region:'us-east-1',credentials:'aws-creds'){
+                    script {
                         sh """
-                            aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
-                            docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
-                            docker images
-                            docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
+                            aws eks update-kubeconfig --region us-east-1 --name ellamma-${PROJECT}-${deploy_to}
+                            kubectl get pods
                         """
                     }
                 }
             }
         }
 
-        stage('Trigger DEV Deploy') {
-            steps{
-                script{
-                    build job: '../catalogue-deploy',
-                        wait: true, // wait for completion
-                        propagate: false, // Propagate status
-                        parameters: [
-                            string(name: 'appVersion', value: "${appVersion}")
-                            string(name: 'deploy_to', value: "dev")
-                            ]
-                }
-            }
-        }
-
-        stage('Deploy') {
-            when { 
-                expression { "$params.DEPLOY" == "true" }
-                }
-
-            steps {
-                echo "Deploying..."
-            }
-        }
-
     }
-           
     post { 
         always { 
             echo 'I will always say Hello again!'
