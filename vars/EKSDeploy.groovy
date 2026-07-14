29fb9c0b20bcc3906d8b0e165f2b4e9f5973d4e2
Shankar-codes <harikainteriors@gmail.com>
2026-07-14 18:18:28 +0530

    Jenkins shared library

--- vars/EKSDeploy.groovy
+++ vars/EKSDeploy.groovy
@@ -1,57 +1,59 @@
-pipeline {
-    agent {
-        node {
-            label 'AGENT-1'
-        }
-    }
-
-    environment { 
-        COURSE = 'Jenkins'
-        appVersion = ""
-        ACC_ID = "367012942501"
-        PROJECT = "roboshop"
-        COMPONENT = "catalogue"
-    }
-
-    options {
-        timeout(time: 1, unit: 'HOURS') 
-        disableConcurrentBuilds()
-    }
-
-    // parameters {
-    //     string(name: 'appVersion', description: 'what app version you want to deploy')
-    //     choice(name: 'deploy_to', choices: ['dev','qa','prod'], description: 'pick something')
-    // }
-
-// this is build section. added comment for just webhook checking
-    stages {
-        stage('Deploy') {
-            steps {
-                withAWS(region:'us-east-1',credentials:'aws-creds'){
-                    script {
-                        sh """
-                            aws eks update-kubeconfig --region us-east-1 --name ellamma-${PROJECT}-${deploy_to}
-                            kubectl get nodes
-                        """
-                    }
-                }
+def call (Map configMap){
+    pipeline {
+        agent {
+            node {
+                label 'AGENT-1'
             }
         }
 
-    }
-    post { 
-        always { 
-            echo 'I will always say Hello again!'
-            cleanWs()
+        environment { 
+            COURSE = 'Jenkins'
+            appVersion = ""
+            ACC_ID = "367012942501"
+            PROJECT = "roboshop"
+            COMPONENT = "catalogue"
         }
-        success{
-            echo "Build and Test completed successfully!"
+
+        options {
+            timeout(time: 1, unit: 'HOURS') 
+            disableConcurrentBuilds()
         }
-        failure{
-            echo "Build or Test failed!"
+
+        // parameters {
+        //     string(name: 'appVersion', description: 'what app version you want to deploy')
+        //     choice(name: 'deploy_to', choices: ['dev','qa','prod'], description: 'pick something')
+        // }
+
+    // this is build section. added comment for just webhook checking
+        stages {
+            stage('Deploy') {
+                steps {
+                    withAWS(region:'us-east-1',credentials:'aws-creds'){
+                        script {
+                            sh """
+                                aws eks update-kubeconfig --region us-east-1 --name ellamma-${PROJECT}-${deploy_to}
+                                kubectl get nodes
+                            """
+                        }
+                    }
+                }
+            }
+
         }
-        aborted{
-            echo "Build or Test aborted!"
+        post { 
+            always { 
+                echo 'I will always say Hello again!'
+                cleanWs()
+            }
+            success{
+                echo "Build and Test completed successfully!"
+            }
+            failure{
+                echo "Build or Test failed!"
+            }
+            aborted{
+                echo "Build or Test aborted!"
+            }
         }
     }
 }
